392e5e70fa8e4c68555089b6366df2e81d91752e
Speed <117187480+plancton0@users.noreply.github.com>
2023-06-04 22:23:43 +0200

    Create messageCreate.js

---
+++ Events/messageCreate.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const cooldowns = new Map();
+const config = require("../config");
+
+module.exports = async (client, message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.prefixCommands.get(commandName);
+  if (!command) return;
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Map());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 0) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(`Veuillez attendre ${timeLeft.toFixed(1)} seconde(s) avant de réutiliser la commande \`${command.name}\`.`);
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+  try {
+    command.execute(message, args, client);
+  } catch (error) {
+    console.error(error);
+    message.reply("Une erreur s'est produite lors de l'exécution de cette commande.");
+  }
+};
