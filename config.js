71bdf35d29d060b1099645497ade3e56c076765e
Speed <117187480+plancton0@users.noreply.github.com>
2023-06-04 22:22:09 +0200

    Update and rename config.js to config.json

--- config.js
+++
@@ -1,9 +0,0 @@
-const owners = ['ID DEV', 'ID DEV',];
-
-module.exports = {
-
-	token: "", // token du bot
-	owners: owners, // id des développeurs du bot
-	color: "", // couleur du bot
-	clientId: "" // id du bot
-}
