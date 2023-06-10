0a37470db75b4920976a1aac704fb10e0d9dfce9
Speed <117187480+plancton0@users.noreply.github.com>
2023-06-10 15:49:54 +0200

    Update interactionCreate.js

--- Events/interactionCreate.js
+++ Events/interactionCreate.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
-const config = require("../config")
-const owners = config.owners;
+require('dotenv').config();
+const owners = process.env.owners
 
 const cooldowns = new Map();
 
